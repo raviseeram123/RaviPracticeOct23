f4bbc71379161e2d6f3fde81d08bc115605c35e6
kumar <kumar@test.com>
2023-10-03 00:12:05 +0530

    Second commit by Kumar

---
+++ src/test/java/addition.java
@@ -0,0 +1,14 @@
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=2;
+		int b=3;
+		int c=a+b;
+		System.out.println("Added value is  "+c);
+		
+	}
+
+}
